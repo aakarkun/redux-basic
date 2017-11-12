b726d8db33dad15897317b978ffef1155cc7d87a
aakarkun <aakar.kun@hotmail.com>
2017-11-13 01:59:05 +0545

    Middleware created

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,33 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 
 const reducer = function(state, action) {
-    if(action.type == "INC") {
+    if(action.type === "INC") {
         return state+action.payload;
     }
-    if(action.type == "DEC") {
+    if(action.type === "DEC") {
         return state-action.payload;
+    } else if(action.type === "E") {
+        throw new Error("Error!");
     }
     return state;
 }
 
-const store = createStore(reducer, 0);
+const logger = (store) => (next) => (action) => {
+    console.log("action fired", action);
+    next(action);
+}
+
+const error = (store) => (next) => (action) => {
+    try {
+        next(action);
+    } catch(e) {
+        console.log("Oops!", e);
+    }
+}
+
+const middleware = applyMiddleware(logger, error);
+
+const store = createStore(reducer, 0, middleware);
 
 store.subscribe(() => {
     console.log("Store changed", store.getState());
@@ -21,3 +38,4 @@ store.dispatch({type: "INC", payload: 5});
 store.dispatch({type: "INC", payload: 5});
 store.dispatch({type: "INC", payload: 5});
 store.dispatch({type: "DEC", payload: 15});
+store.dispatch({type: "E", payload: 15});
