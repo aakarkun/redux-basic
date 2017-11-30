c83ac06fc0f45f74cda8778fec8e41b2efa015bc
aakarkun <aakar.kun@hotmail.com>
2017-11-30 14:31:12 +0545

    update

--- src/js/store.js
+++ src/js/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware,createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promise from 'redux-promise-middleware'
@@ -8,5 +8,3 @@ import reducer from './reducers'
 const middleware = applyMiddleware(promise(), thunk, logger());
 
 export default createStore(reducer, middleware);
-
-
